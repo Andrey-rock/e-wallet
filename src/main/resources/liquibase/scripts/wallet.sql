1250d4aca99f6b9a9215b97b83b73932371e47c9
Andrey-rock <andrey-rok@maol.ru>
2025-08-12 21:39:15 +0300

    Доработан docker-compose. Добавлен скрипт заполняющий БД. Исправлена блокировка транзакций с READ на WRITE

--- src/main/resources/liquibase/scripts/wallet.sql
+++ src/main/resources/liquibase/scripts/wallet.sql
@@ -6,3 +6,12 @@ CREATE TABLE wallets
     wallet_id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     balance    NUMERIC          DEFAULT 0
 );
+
+-- changeset andrey-rock:2
+
+insert into wallets(wallet_id, balance)
+VALUES ('cda242bc-8b4e-4d3f-ba09-974fb6cce200', 1000),
+       ('cda242bc-8b4e-4d3f-ba09-974fb6cce210', 1000),
+       ('cda242bc-8b4e-4d3f-ba09-974fb6cce220', 1000),
+       ('cda242bc-8b4e-4d3f-ba09-974fb6cce230', 1000),
+       ('cda242bc-8b4e-4d3f-ba09-974fb6cce240', 1000);
